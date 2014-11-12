f214c3d08a5f171adf33090314c7050bfb51f9eb
jalal <subluq@gmail.com>
2014-11-12 11:14:24 +0700

    add brew.sh and deploy.sh

---
+++ brew.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.osx` has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Install Brew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Install `wget` with IRI support.
+brew install wget --with-iri
+
+# Install more recent versions of some OS X tools.
+brew install vim --override-system-vi
+
+# Install other useful binaries.
+brew install youtube-dl
+brew install rename
+brew install dfc
+brew install tree
+brew install htop
+brew install pidof
+
+# Install mpd, mpdscribble, ncmpcpp, and mpc
+brew install mpd
+brew install ncmpcpp
+brew install mpdscribble
+brew install mpd
+
+# Remove outdated versions from the cellar.
+brew cleanup
